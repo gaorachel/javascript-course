3fca8ab5d9c91ba21b09509c5432f282688b9610
Rachel <rachel.gao@samarkand.global>
2023-03-01 20:48:41 +0000

    Initial commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,31 @@
+// let js = 'amazing';
+// // if (js === 'amazing') alert('js is fun!');
+// console.log("biuuuuuuu")
+
+
+// let num = 23;
+// console.log(num)
+
+
+
+/**
+ * Assignment 1
+ * LECTURE: Values and Variables
+ * 1. Declarevariablescalled'country','continent'and'population'and assign their values according to your own country (population in 
+ * millions)
+2. Logtheirvaluestotheconsole
+ */
+
+let country = "China"
+let continent = "Asia"
+let population = '14M'
+
+/**
+ * @param input string
+ */
+const someFunction = (input) => {
+    console.log('hello')
+}
+
+
+console.log(country, continent, population)
