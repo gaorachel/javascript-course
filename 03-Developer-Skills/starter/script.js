04f445c0fc548b149f6ac7929e88a1a0bf7279d7
Rachel <gggggrea@gmail.com>
2023-03-04 23:24:43 +0000

    Learn basic HTML & CSS

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,19 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const abcd = 23;
+
+const calAge = (brithYear) => 2023 - brithYear;
+let age = calAge(1993);
+console.log(age);
+
+console.log();
+
+console.log(abcd);
+console.log();
+
+console.log(abcd);
+
+// BUG
+console.log(100000);
+console.log("test");
