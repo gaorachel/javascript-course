a2aac6525e7aaf18c0bb42ef025eb630b33fa731
Rachel <gggggrea@gmail.com>
2023-03-06 20:17:38 +0000

    First project - guessing my number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,60 @@
-'use strict';
+"use strict";
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "Correct Number!";
+// console.log(document.querySelector(".message").textContent);
+
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+// document.querySelector(".guess").value = 22;
+
+// console.log((document.querySelector(".guess").value = 22));
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1; // get a random number between 1 and 20
+// console.log(secretNumber);
+let score = 20;
+let highscore = 0;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number!";
+
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    } else {
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 0) {
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "Too High!" : "Too Low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "GAME OVER!";
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".message").textContent = "Start guessing...";
+
+  document.querySelector(".score").textContent = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  //   console.log(secretNumber);
+
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").textContent = "?";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
