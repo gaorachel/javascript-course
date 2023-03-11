a4def22ed1fe333d833fe09cb8b6b234d2e6a33d
Rachel <gggggrea@gmail.com>
2023-03-11 18:54:53 +0000

    Learn the for of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -60,69 +60,73 @@ console.log(arr);
 console.log(...arr);
 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+for (const item of menu) console.log(item);
+for (const item of menu.entries()) console.log(item);
+for (const [i, el] of menu.entries()) console.log(`${i + 1}: ${el}`);
 
 const str = "abcdefg";
 console.log(...str);
 
-/////////////////// Coding Challenge
+// /////////////////// Coding Challenge
 
-const game = {
-  team1: "Bayern Munich",
-  team2: "Borrussia Dortmund",
-  players: [
-    [
-      "Neuer",
-      "Pavard",
-      "Martinez",
-      "Alaba",
-      "Davies",
-      "Kimmich",
-      "Goretzka",
-      "Coman",
-      "Muller",
-      "Gnarby",
-      "Lewandowski",
-    ],
-    [
-      "Burki",
-      "Schulz",
-      "Hummels",
-      "Akanji",
-      "Hakimi",
-      "Weigl",
-      "Witsel",
-      "Hazard",
-      "Brandt",
-      "Sancho",
-      "Gotze",
-    ],
-  ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-  date: "Nov 9th, 2037",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: "Bayern Munich",
+//   team2: "Borrussia Dortmund",
+//   players: [
+//     [
+//       "Neuer",
+//       "Pavard",
+//       "Martinez",
+//       "Alaba",
+//       "Davies",
+//       "Kimmich",
+//       "Goretzka",
+//       "Coman",
+//       "Muller",
+//       "Gnarby",
+//       "Lewandowski",
+//     ],
+//     [
+//       "Burki",
+//       "Schulz",
+//       "Hummels",
+//       "Akanji",
+//       "Hakimi",
+//       "Weigl",
+//       "Witsel",
+//       "Hazard",
+//       "Brandt",
+//       "Sancho",
+//       "Gotze",
+//     ],
+//   ],
+//   score: "4:0",
+//   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+//   date: "Nov 9th, 2037",
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-const [players1, players2] = game.players;
-console.log([players1, players2]);
+// const [players1, players2] = game.players;
+// console.log([players1, players2]);
 
-const [gk, ...fieldPlayers] = players1;
-console.log([gk, fieldPlayers]);
+// const [gk, ...fieldPlayers] = players1;
+// console.log([gk, fieldPlayers]);
 
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
 
-const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
-console.log(players1Final);
+// const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+// console.log(players1Final);
 
-const { team1, draw, team2 } = game.odds;
-console.log(team1, draw, team2);
+// const { team1, draw, team2 } = game.odds;
+// console.log(team1, draw, team2);
 
-const printGoals = function () {
-  let playerNum = Math.trunc(Math.random * 22 + 1);
-  console.log();
-};
+// const printGoals = function () {
+//   let playerNum = Math.trunc(Math.random * 22 + 1);
+//   console.log();
+// };
